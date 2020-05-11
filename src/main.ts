6e971e1d472a4b0148f3742f94a9572e65f1a817
Алексей Лейбиченко <a.leybichenko@MacBook-Pro-admin.local>
2020-05-11 20:09:17 +0300

    use config service in mait, small swagger refactor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ConfigService } from './config/config.service';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix(configService.globalApiPrefix);
+
+  const options = new DocumentBuilder()
+    .setTitle("Taxi service")
+    .setDescription("The Taxi Service API description")
+    .setBasePath(`/${configService.globalApiPrefix}`)
+    .setVersion("1.0")
+    .addTag("taxi")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  console.log(`${configService.globalApiPrefix}/swagger-docs`);
+  SwaggerModule.setup(`${configService.globalApiPrefix}/swagger-docs`, app, document);
+
+
+  await app.listen(configService.serverPort, () => console.log(`Server successfully started on port: ${configService.serverPort}`));
 }
 bootstrap();
